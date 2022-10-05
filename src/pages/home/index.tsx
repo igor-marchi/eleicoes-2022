d0d5e7feb04c7f874af24ddde0b873a51998fcf5
Igor Marchi <igoraugustomarchi@gmail.com>
2022-10-04 22:42:58 -0300

    rename data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,37 +2,64 @@ import { useEffect, useState } from "react";
 import { HomeContainer } from "./styles";
 import { formatNumber, formatNumberCompact } from "../../utils/formatter";
 
+interface CandidateJSON {
+  cc: string;
+  nm: string;
+  pvap: string;
+  vap: number;
+  n: string;
+}
 interface Candidate {
-  cc: string,
-  nm: string,
-  pvap: string,
-  vap: number,
-  n: string
+  party: string;
+  name: string;
+  votesInPercent: string;
+  votes: number;
+  number: string;
+}
+
+function replyDataNames(data: CandidateJSON[]): Candidate[] {
+  return data.map((candidateJson) => {
+    const candidate: Candidate = {
+      name: candidateJson.nm,
+      number: candidateJson.n,
+      party: candidateJson.cc,
+      votes: candidateJson.vap,
+      votesInPercent: candidateJson.pvap,
+    };
+
+    return candidate;
+  });
 }
 
 export function Home() {
-  const [candidates, setCandidates ] = useState<Candidate[]>([]);
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
 
   useEffect(() => {
-    fetch('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json')
-    .then(response => response.json())
-    .then(data => setCandidates(data.cand))
-  },[])
+    fetch(
+      "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setCandidates(replyDataNames(data.cand)));
+  }, []);
 
   return (
     <HomeContainer>
       <h1>Candidatos</h1>
       {candidates.map((candidate) => {
         return (
-          <div key={candidate.n}>
-            <p>Número {candidate.n}</p>
-            <p>Nome {candidate.nm}</p>
-            <p>Partido {candidate.cc}</p>
-            <p title={formatNumberCompact(candidate.vap)}>Votos {formatNumber(candidate.vap)}</p>
-            <p>{candidate.pvap} %</p>
+          <div key={candidate.number}>
+            <p>Número {candidate.number}</p>
+            <p>Nome {candidate.name}</p>
+            <p>Partido {candidate.party}</p>
+            <p title={formatNumberCompact(candidate.votes)}>
+              Votos {formatNumber(candidate.votes)}
+            </p>
+            <p>Porcentagem de votos: {candidate.votesInPercent} %</p>
+
+            <hr />
           </div>
-        )
+        );
       })}
     </HomeContainer>
-  )
+  );
 }
